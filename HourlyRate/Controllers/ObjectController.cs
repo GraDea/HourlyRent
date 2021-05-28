115f7bff9a4dedf48fb6f7179c8f94d2b242cf31
Nikita Komarov <n.komarov@cian.ru>
2021-05-28 16:10:07 +0300

    add filter

--- HourlyRate/Controllers/ObjectController.cs
+++ HourlyRate/Controllers/ObjectController.cs
@@ -1,8 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 using HourlyRate.Data;
 using HourlyRate.Data.Models;
+using HourlyRate.Utility;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -24,11 +27,24 @@ namespace HourlyRate.Controllers
         }
 
 
-        [HttpGet()]
-        [Route("api/[controller]/find")]
-        public async Task<IEnumerable<RealtyObject>> FindClient()
+        [HttpGet]
+        [Route("api/[controller]/get-all")]
+        public async Task<IEnumerable<RealtyObject>> GetAll()
         {
             return await this.objects.ToListAsync();
+        }
+        
+        [HttpPost]
+        [Route("api/[controller]/find")]
+        public async Task<IEnumerable<RealtyObject>> Filter(ObjectsFilter filter)
+        {
+            Expression<Func<RealtyObject, bool>> expression = c => true;
+
+            if (!string.IsNullOrEmpty(filter?.Region))
+            {
+                expression = expression.And(c => c.Region == filter.Region);
+            }
+            return await this.objects.Where(expression).ToListAsync();
         }
 
 
@@ -38,5 +54,37 @@ namespace HourlyRate.Controllers
         {
             return await this.objects.FirstAsync(o => o.Id == id);
         }
+    }
+
+    public class ObjectsFilter
+    {
+        public SquareFilter Square { get; set; }
+
+        public PriceFilter Price { get; set; }
+        
+        public DateFilter Date { get; set; }
+        
+        public string Region { get; set; }
+    }
+
+    public class DateFilter
+    {
+        public DateTime? From { get; set; }
+        
+        public DateTime? To { get; set; }
+    }
+
+    public class PriceFilter
+    {
+        public decimal? From { get; set; }
+        
+        public decimal? To { get; set; }
+    }
+
+    public class SquareFilter
+    {
+        public double? From { get; set; }
+        
+        public double? To { get; set; }
     }
 }
