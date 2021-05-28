4dc6b467011988630d5f09a0c2ff62a999161366
sdmitriev1987 <s.dmitriev@cian.ru>
2021-05-28 10:16:52 +0300

    Swagger

---
+++ HourlyRate/Controllers/MainController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HourlyRate.Controllers
+{
+    public class MainController : Controller
+    {
+        [HttpDelete("{id}")]
+        public void Index(int id)
+        {
+            
+        }
+    }
+}
