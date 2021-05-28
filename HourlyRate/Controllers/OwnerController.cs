9362e34b49cbd7abb321489680cf5b6f0b8b8fc7
Evgeniy Peshkov <e.peshkov@gmail.com>
2021-05-28 14:42:36 +0300

    photo upload

--- HourlyRate/Controllers/OwnerController.cs
+++ HourlyRate/Controllers/OwnerController.cs
@@ -1,11 +1,20 @@
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Reflection.Metadata;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Storage.Blobs;
 using HourlyRate.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HourlyRate.Controllers
 {
     public class OwnerController : Controller
     {
+        public string ConnectionString = "DefaultEndpointsProtocol=https;AccountName=hourlyrentphotos;AccountKey=v7UBtaQSmDxQRFBY50MmOSsRL6bJgPhqpC2TQ7vx68TaigXYFvQ0+yl+Eblav28gcq0Veo2wKP0x8RVlSPu/5A==;EndpointSuffix=core.windows.net";
+        
         public IActionResult EditObject(RealEstateObject realEstateObject)
         {
             return RedirectToAction("Object", new {id = realEstateObject.Id});
@@ -21,13 +30,28 @@ namespace HourlyRate.Controllers
 
                 In hac habitasse platea dictumst. Vestibulum consectetur in turpis pellentesque dignissim. Vestibulum a justo ac tortor rutrum fermentum. Aliquam rutrum venenatis massa a mattis. Nam vel lacus eget mi commodo mattis. Suspendisse euismod nulla at sapien semper posuere. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum ut mi lectus. Morbi congue neque id dictum vestibulum. Nunc semper accumsan felis in eleifend. Nunc enim augue, sollicitudin eget ultrices eget, maximus et lorem. Phasellus volutpat ligula suscipit congue rutrum. Aliquam luctus luctus ante sit amet hendrerit.",
                 Title = "Лофт Ле Форт",
-                Photos = new[] {new Photo() {Url = "https://bc-lefort.ru/xml/Photos/317873.jpg"}}
+                Photos = new[]
+                {
+                    new Photo() {Url = "https://bc-lefort.ru/xml/Photos/317873.jpg"},
+                    new Photo() {Url = "https://hourlyrentphotos.blob.core.windows.net/photos/88a5d965-3c78-45af-9eee-1f1282d1828f-1_YAaAjBYEDVI7ynN588t8fg.jpeg"},
+                }
             });
         }
 
-        public IActionResult UploadPhoto()
+        public async Task<IActionResult> UploadPhoto(int id, IFormFile file)
         {
-            throw new System.NotImplementedException();
+            await using var memoryStream = new MemoryStream();
+            await file.CopyToAsync(memoryStream);
+            memoryStream.Position = 0;
+
+            var client = new BlobContainerClient(ConnectionString, "photos");
+            var name = $"{Guid.NewGuid().ToString()}-{file.FileName}";
+            var response = await client.UploadBlobAsync(name, memoryStream);
+            
+            
+
+            return Json($"{client.Uri}/{name}");
+            //return RedirectToAction("Object", new {id});
         }
     }
 }
