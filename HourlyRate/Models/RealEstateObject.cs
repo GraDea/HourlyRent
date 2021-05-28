6eeb653778af572c47427be30e9c4a5d5d1acad9
Evgeniy Peshkov <e.peshkov@gmail.com>
2021-05-28 12:21:29 +0300

    вьюшка для списка объектов

---
+++ HourlyRate/Models/RealEstateObject.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HourlyRate.Models
+{
+    public class RealEstateObject
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public IReadOnlyCollection<Photo> Photos { get; set; }
+
+        public string ThumbnailUrl => Photos.FirstOrDefault()?.Url;
+    }
+
+    public class Photo
+    {
+        public string Url { get; set; }
+    }
+}
